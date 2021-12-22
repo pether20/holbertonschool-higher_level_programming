b0b2404296ff49493290587bcb574796578005ec
pether20 <pether.te.z@hotmail.com>
2021-12-21 20:14:15 -0500

    Add new script of python if else loops

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+	r = number % -10
+else:
+	r = number % 10
+print("Last digit of", number, "is", r, end=' ')
+if r > 5:
+	print('and is greater than 5')
+elif r == 0:
+	print("and is 0")
+else:
+	print("and is less than 6 and not 0")
