a059dae2d8d44ecc2253584940664755f5625b78
pether20 <pether.te.z@hotmail.com>
2021-12-26 04:26:01 -0500

    Add task 2 of python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-  import sys
-  if len(sys.argv)  == 1:
-    print(0, "arguments")
-  if len(sys.argv) == 2:
-    print(1, "argument")
-    print("1:", sys.argv[1])
-  if len(sys.argv) > 2:
-    print(len(sys.argv)-1, "arguments")
-    for i in range(1, len(sys.argv)):
-      print(f"{i}: {sys.argv[i]}")
+    import sys
+    if len(sys.argv) == 1:
+        print(0, "arguments")
+    elif len(sys.argv) == 2:
+        print(1, "argument")
+        print("1:", sys.argv[1])
+    else:
+        print(len(sys.argv)-1, "arguments")
+        for i in range(1, len(sys.argv)):
+            print(f"{i}: {sys.argv[i]}")
