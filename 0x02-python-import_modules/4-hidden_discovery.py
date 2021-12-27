23fb048116c7afd621f4275ba35eb80ccf9c1f8e
pether20 <pether.te.z@hotmail.com>
2021-12-27 02:24:52 -0500

    Add task 4 of python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3.8
 if __name__ == "__main__":
     from hidden_4 import *
-    for s in dir():
-        if s[:2] != "__":
-            print("{}".format(s))
+    for a in dir():
+        if a[:2] != "__":
+            print(f"{a}")
