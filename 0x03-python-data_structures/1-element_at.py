32e91669c0ac5d5bedb48d822a82f2e7148688a9
Codeparker <codeparker@localhost.localdomain>
2022-01-03 12:32:31 -0500

    Add return None in function

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx == i:
-            return i + 1
+    if idx < 0 or idx >= len(my_list):
+        return None 
+    return my_list[idx]
