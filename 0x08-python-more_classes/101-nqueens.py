ba46178e08e16e9b67fd1439c8e4c0ecee6390d0
pether20 <pether.te.z@hotmail.com>
2022-01-26 19:12:53 -0500

    Add task 11 python more class and objects

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-queens puzzle
+The N queens puzzle is the challenge of placing N non-attacking queens
+on an N×N chessboard. Write a program that solves the N queens problem.
 """
 
 
