e7fda729b7bdd4aa84f797c84665af3e925a9d54
pether20 <pether.te.z@hotmail.com>
2022-01-26 19:19:18 -0500

    Add task 11 python more class and objects

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -8,7 +8,7 @@ on an N×N chessboard. Write a program that solves the N queens problem.
 from sys import argv
 
 if __name__ == "__main__":
-    li = []
+    a = []
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
@@ -22,22 +22,22 @@ if __name__ == "__main__":
 
     # initialize the answer list
     for i in range(n):
-        li.append([i, None])
+        a.append([i, None])
 
     def already_exists(y):
         """check that a queen does not already exist in that y value"""
         for x in range(n):
-            if y == li[x][1]:
+            if y == a[x][1]:
                 return True
         return False
 
     def reject(x, y):
         """determines whether or not to reject the solution"""
-        if already_exists(y):
+        if (already_exists(y)):
             return False
         i = 0
-        while i < x:
-            if abs(li[i][1] - y) == abs(i - x):
+        while(i < x):
+            if abs(a[i][1] - y) == abs(i - x):
                 return False
             i += 1
         return True
@@ -45,17 +45,17 @@ if __name__ == "__main__":
     def clear_a(x):
         """clears the answers from the point of failure on"""
         for i in range(x, n):
-            li[i][1] = None
+            a[i][1] = None
 
     def nqueens(x):
         """recursive backtracking function to find the solution"""
         for y in range(n):
             clear_a(x)
             if reject(x, y):
-                li[x][1] = y
+                a[x][1] = y
                 # accepts the solution
-                if x == n - 1:
-                    print(li)
+                if (x == n - 1):
+                    print(a)
                 # moves on to next x value to continue
                 else:
                     nqueens(x + 1)
