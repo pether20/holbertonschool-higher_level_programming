c0e861f440fd966997d833d08bc84f11cd67c423
pether20 <pether.te.z@hotmail.com>
2022-01-26 20:09:58 -0500

    Add parents for functions

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,77 +1,51 @@
 #!/usr/bin/python3
-"""
-The N queens puzzle is the challenge of placing N non-attacking queens
-on an N×N chessboard. Write a program that solves the N queens problem.
-"""
+"""N Queens problem"""
 
 
-from sys import argv
+def isSafe(bo, row, col):
+    """Checks if position is safe from attack.
+    Args:
+        bo: The board state.
+        row: The row to check.
+        col: The colum to check.
+    """
+    for c in range(col):
+        if bo[c] is row or abs(bo[c] - row) is abs(c - col):
+            return False
+    return True
+
+
+def checkBoard(bo, col):
+    """Checks the board state column by column using backtracking.
+    Args:
+        bo: The board state.
+        col: The current colum to check.
+    """
+    nm = len(bo)
+    if col is nm:
+        print(str([[c, bo[c]] for c in range(nm)]))
+        return
+
+    for row in range(nm):
+        if isSafe(bo, row, col):
+            bo[col] = row
+            checkBoard(bo, col + 1)
+
 
 if __name__ == "__main__":
-    # !/usr/bin/python3
-    """
-    The N queens puzzle is the challenge of placing N non-attacking queens
-    on an N×N chessboard. Write a program that solves the N queens problem.
-    """
+    import sys
 
-    from sys import argv
-
-    if __name__ == "__main__":
-        a = []
-        if len(argv) != 2:
-            print("Usage: nqueens N")
-            exit(1)
-        if argv[1].isdigit() is False:
-            print("N must be a number")
-            exit(1)
-        n = int(argv[1])
-        if n < 4:
-            print("N must be at least 4")
-            exit(1)
-
-        # initialize the answer list
-        for i in range(n):
-            a.append([i, None])
-
-
-        def already_exists(y):
-            """check that a queen does not already exist in that y value"""
-            for x in range(n):
-                if y == a[x][1]:
-                    return True
-            return False
-
-
-        def reject(x, y):
-            """determines whether or not to reject the solution"""
-            if (already_exists(y)):
-                return False
-            i = 0
-            while (i < x):
-                if abs(a[i][1] - y) == abs(i - x):
-                    return False
-                i += 1
-            return True
-
-
-        def clear_a(x):
-            """clears the answers from the point of failure on"""
-            for i in range(x, n):
-                a[i][1] = None
-
-
-        def nqueens(x):
-            """recursive backtracking function to find the solution"""
-            for y in range(n):
-                clear_a(x)
-                if reject(x, y):
-                    a[x][1] = y
-                    # accepts the solution
-                    if (x == n - 1):
-                        print(a)
-                    # moves on to next x value to continue
-                    else:
-                        nqueens(x + 1)
-
-
-        nqueens(0)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    n = 0
+    try:
+        n = int(sys.argv[1])
+    except:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    board = [0 for col in range(n)]
+    checkBoard(board, 0)
