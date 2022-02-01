dfe568ac87bd962fc960c0a431700ac6b4353308
pether20 <pether.te.z@hotmail.com>
2022-02-01 12:59:27 -0500

    Add task 01 python print number characters

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" task 01 read file"""
+
+
+def write_file(filename="", text=""):
+    """Methode write file"""
+    with open(filename, 'w', encoding="UTF8") as f:
+        print(f.write(text), end="")
