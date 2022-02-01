c668c76f03102aa50815e4e9a5f1a46ca75286a2
pether20 <pether.te.z@hotmail.com>
2022-02-01 18:24:44 -0500

    Add task 02 python

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-task 01 read file
-"""
+""" task 01 read file"""
 
 
 def append_write(filename="", text=""):
     """Methode write file"""
-    with open(filename, 'w', encoding="UTF8") as f:
+    with open(filename, 'r+', encoding="UTF8") as f:
         return f.write(text)
