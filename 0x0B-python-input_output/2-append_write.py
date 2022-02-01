1e58b4bbc19ba7a34dfd5a4a244e98c55ed53015
pether20 <pether.te.z@hotmail.com>
2022-02-01 13:15:12 -0500

    Add task 02 python return number characters

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" task 01 read file"""
+
+
+def append_write(filename="", text=""):
+    """Methode write file"""
+    with open(filename, 'w', encoding="UTF8") as f:
+        return f.write(text)
