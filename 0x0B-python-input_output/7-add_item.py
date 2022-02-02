f2a8f06361639c7484b54d8600c5559924988d74
pether20 <pether.te.z@hotmail.com>
2022-02-01 23:48:43 -0500

    Add task 07 python

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """ task 07 read file"""
-
-
 from sys import argv
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-filename = "add_item.json"
-
 try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
+    json_list = load_from_json_file('add_item.json')
+except:
+    json_list = []
 
-save_to_json_file(existing_content + argv[1:], filename)
+for i in argv[1:]:
+    json_list.append(i)
+
+save_to_json_file(json_list, 'add_item.json')
