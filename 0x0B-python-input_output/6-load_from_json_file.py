bbb429ad3d8562c8a31eba763d753a7feab6d86e
pether20 <pether.te.z@hotmail.com>
2022-02-01 23:10:39 -0500

    Add task 06 python

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,6 +5,5 @@
 def load_from_json_file(filename):
     """Methode load fron json file"""
     import json
-    with open(filename, 'w', encoding="UTF8") as f:
+    with open(filename, "r", encoding="UTF8") as f:
         return json.load(f)
-
