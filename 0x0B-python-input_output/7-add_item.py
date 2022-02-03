6c5f61313488566f24ec753fea651680f8a51282
pether20 <pether.te.z@hotmail.com>
2022-02-02 23:59:34 -0500

    Add tasks  python

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" task 07 read file"""
-from sys import argv
+"""Function """
+import sys
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 try:
-    json_list = load_from_json_file('add_item.json')
+    js_list = load_from_json_file('add_item.json')
 except:
-    json_list = []
+    js_list = []
 
-for i in argv[1:]:
-    json_list.append(i)
+for arg in sys.argv[1:]:
+    js_list.append(arg)
 
-save_to_json_file(json_list, 'add_item.json')
+save_to_json_file(js_list, "add_item.json")
