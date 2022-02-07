42fc1c5d19fec159f74528ecc354fbf7040d318f
pether20 <pether.te.z@hotmail.com>
2022-02-07 12:34:21 -0500

    Add new function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,14 +3,14 @@
 
 
 class Base:
-    """ Class Base"""
+	""" Class Base"""
 
-    __nb_objects = 0
+	__nb_objects = 0
 
-    def __init__(self, id=None):
+	def __init__(self, id=None):
 
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+		if id is not None:
+			self.id = id
+		else:
+			Base.__nb_objects += 1
+			self.id = Base.__nb_objects
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,44 +1,62 @@
 #!/usr/bin/python3
 """ FUNCTION RECTANGLE """
+from models.base import Base
 
+class Rectangle(Base):
+	""" Class Rectangle inherits from Base """
 
-class Rectangle(BASE):
-    """ Class Rectangle inherits from Base """
+	def __init__(self, width, height, x=0, y=0, id=None):
+		super().__init__(id)
+		self.width = width
+		self.height = height
+		self.x = x
+		self.y = y
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+	@property
+	def width(self):
+		"""Set/Get the width of the Rectangle"""
+		return self.__width
 
-    @property
-    def width(self):
-        """Set/Get the width of the Rectangle"""
-        return self.__width
+	@property
+	def height(self):
+		return self.__height
 
-    @property
-    def height(self):
-        return self.__height
+	@property
+	def x(self):
+		return self.__x
 
-    @property
-    def x(self):
-        return self.__x
+	@property
+	def y(self):
+		return self.__y
 
-    @property
-    def y(self):
-        return self__y
+	@width.setter
+	def width(self, value):
+		if not (isinstance(value, int)):
+			raise TypeError('width must be an integer')
+		if value <= 0:
+			raise ValueError('width must be > 0')
+		self.__width = value
 
-    @width.setter
-    def width(self, value):
-        self.__width = value
-    
-    @height.setter
-    def height(self, value):
-        self.__height = value
+	@height.setter
+	def height(self, value):
+		if not (isinstance(value, int)):
+			raise TypeError('height must be an integer')
+		if value <= 0:
+			raise ValueError('height must be > 0')
+		self.__height = value
 
-    @x.setter
-    def x(self, value):
-        
+	@x.setter
+	def x(self, value):
+		if not (isinstance(value, int)):
+			raise TypeError('x must be an integer')
+		if value < 0:
+			raise ValueError('x must be >= 0')
+		self.__x = value
 
-        
+	@y.setter
+	def y(self, value):
+		if not (isinstance(value, int)):
+			raise TypeError('y must be an integer')
+		if value < 0:
+			raise ValueError('y must be >= 0')
+		self.__y = value
